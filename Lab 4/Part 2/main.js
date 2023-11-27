502614a8396ea5776a3e5eacb11ca13f99ae41ca
Santoshi Lamichhane <144840354+Santoshilc@users.noreply.github.com>
2023-11-27 15:29:48 -0500

    COMMIT-2
    HTML COMMNETS ADDED

--- Lab 4/Part 2/main.js
+++ Lab 4/Part 2/main.js
@@ -5,7 +5,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const arrayImages = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const alternativeText = {
+  'pic1.jpg' : 'humman eye',
+  'pic2.jpg' : 'Rocks',
+  'pic3.jpg' : 'new flowers',
+  'pic4.jpg' : 'tomb',
+  'pic5.jpg' : 'moth on leaf'
+}
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
