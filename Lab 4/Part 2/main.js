8025262a818402378c72e7eb6de608b174f6ff13
Santoshi Lamichhane <144840354+Santoshilc@users.noreply.github.com>
2023-11-27 15:36:22 -0500

    commit-4
    loping through the image and clickEvent button added.

--- Lab 4/Part 2/main.js
+++ Lab 4/Part 2/main.js
@@ -1,22 +1,33 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const arrayImages = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
-const alternativeText = {
-  'pic1.jpg' : 'humman eye',
-  'pic2.jpg' : 'Rocks',
-  'pic3.jpg' : 'new flowers',
-  'pic4.jpg' : 'tomb',
-  'pic5.jpg' : 'moth on leaf'
-  
-}
+const imageArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
 /* Declaring the alternative text for each image file */
+const altText = {
+  'pic1.jpg': 'human eye',
+  'pic2.jpg': 'Rocks',
+  'pic3.jpg': 'new flowers',
+  'pic4.jpg': 'tomb',
+  'pic5.jpg': 'moth on leaf'
+};
 
 /* Looping through images */
+for (const imageName of imageArray) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${imageName}`);
+  newImage.setAttribute('alt', altText[imageName]);
+  thumbBar.appendChild(newImage);
+
+  newImage.addEventListener('click', (e) => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
+
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
