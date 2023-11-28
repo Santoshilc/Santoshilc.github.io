523bbcafee40f091a268c2312a2acbdf6b0ec43b
Santoshi Lamichhane <144840354+Santoshilc@users.noreply.github.com>
2023-11-27 21:33:48 -0500

    Commit-2
    Shape class created and update the collision detection part

--- Lab 4/Part 4/main-finished.js
+++ Lab 4/Part 4/main-finished.js
@@ -17,10 +17,20 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+class Shape {
+   constructor(x, y, velX, velY) {
+      this.x = x;
+      this.y = y;
+      this.velX = velX;
+      this.velY = velY;
+   }
+}
 
-class Ball {
+class Ball extends Shape{
 
    constructor(x, y, velX, velY, color, size) {
+      super(x, y, velX, velY);
+
       this.x = x;
       this.y = y;
       this.velX = velX;
@@ -57,19 +67,19 @@ class Ball {
       this.y += this.velY;
    }
 
-   collisionDetect() {
+   ccollisionDetect() {
       for (const ball of balls) {
-         if (!(this === ball)) {
-            const dx = this.x - ball.x;
-            const dy = this.y - ball.y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-
-            if (distance < this.size + ball.size) {
-              ball.color = this.color = randomRGB();
-            }
-         }
+        if (!(this === ball) && ball.exists) {
+          const dx = this.x - ball.x;
+          const dy = this.y - ball.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+    
+          if (distance < this.size + ball.size) {
+            ball.color = this.color = randomRGB();
+          }
+        }
       }
-   }
+    }
 
 }
 
