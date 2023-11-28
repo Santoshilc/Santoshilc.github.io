b653750eb7bab72a9bbf8b65821cd8ea47ee81d0
Santoshi Lamichhane <144840354+Santoshilc@users.noreply.github.com>
2023-11-27 20:41:52 -0500

    Ball class created

--- Lab 4/Part 3/main.js
+++ Lab 4/Part 3/main.js
@@ -1,20 +1,30 @@
 // setup canvas
 
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
 // function to generate random number
-
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
 
 // function to generate random color
-
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+// Creating a class named ball so that we can have multiple balls as its object and intilizing some of its properties
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velocityX;
+    this.velY = velocityY;
+    this.color = color;
+    this.size = size;
+  }
 }
