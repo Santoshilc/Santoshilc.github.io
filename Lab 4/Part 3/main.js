6e4c1b526f2cc0c93addafba06f7d6cbd3231ba4
Santoshi Lamichhane <144840354+Santoshilc@users.noreply.github.com>
2023-11-27 21:08:26 -0500

    Commit-4
    Use while loop to create balls on screen

--- Lab 4/Part 3/main.js
+++ Lab 4/Part 3/main.js
@@ -57,3 +57,30 @@ class Ball {
     this.y += this.velocityY;
   }
 }
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-6, 6),
+    random(-6, 6),
+    randomRGB(),
+    size,
+  );
+
+  balls.push(ball);
+}
+ function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.drawball();
+    ball.update();
+  }
+
+  requestAnimationFrame(loop);
+}
+
